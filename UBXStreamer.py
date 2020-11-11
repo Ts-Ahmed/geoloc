4271aa5f4ef1e644d8d4acec055853778019c724
Ahmed Tsoroev <ts.ahmed.edu@gmail.com>
2020-11-11 15:22:55 +0100

    Separate files
    Signed-off-by: Ahmed Tsoroev <ts.ahmed.edu@gmail.com>

---
+++ UBXStreamer.py
@@ -0,0 +1,123 @@
+from io import BufferedReader
+from threading import Thread
+
+from pyubx2.ubxreader import UBXReader
+from serial import Serial, SerialException, SerialTimeoutException
+
+import pyubx2.exceptions as ube
+
+
+class UBXStreamer:
+    """
+    UBXStreamer class.
+    """
+
+    def __init__(self, port, baudrate, timeout=5):
+        """
+        Constructor.
+        """
+
+        self._serial_object = None
+        self._serial_thread = None
+        self._ubxreader = None
+        self._connected = False
+        self._reading = False
+        self._port = port
+        self._baudrate = baudrate
+        self._timeout = timeout
+        self.ephemeris = None
+
+    def __del__(self):
+        """
+        Destructor.
+        """
+
+        self.stop_read_thread()
+        self.disconnect()
+
+    def connect(self):
+        """
+        Open serial connection.
+        """
+
+        try:
+            self._serial_object = Serial(self._port,
+                                         self._baudrate,
+                                         timeout=self._timeout)
+            self._ubxreader = UBXReader(BufferedReader(self._serial_object), False)
+            self._connected = True
+        except (SerialException, SerialTimeoutException) as err:
+            print(f"Error connecting to serial port {err}")
+
+    def disconnect(self):
+        """
+        Close serial connection.
+        """
+
+        if self._connected and self._serial_object:
+            try:
+                self._serial_object.close()
+            except (SerialException, SerialTimeoutException) as err:
+                print(f"Error disconnecting from serial port {err}")
+        self._connected = False
+
+    def start_read_thread(self):
+        """
+        Start the serial reader thread.
+        """
+
+        if self._connected:
+            self._reading = True
+            self._serial_thread = Thread(target=self._read_thread, daemon=False)
+            self._serial_thread.start()
+
+    def stop_read_thread(self):
+        """
+        Stop the serial reader thread.
+        """
+
+        if self._serial_thread is not None:
+            self._reading = False
+            self._serial_thread.join()
+
+    def send(self, data):
+        """
+        Send data to serial connection.
+        """
+
+        self._serial_object.write(data)
+
+    def flush(self):
+        """
+        Flush input buffer
+        """
+
+        self._serial_object.reset_input_buffer()
+
+    def waiting(self):
+        """
+        Check if any messages remaining in the input buffer
+        """
+
+        return self._serial_object.in_waiting
+
+    def _read_thread(self):
+        """
+        THREADED PROCESS
+        Reads and parses UBX message data from stream
+        """
+
+        while self._reading and self._serial_object:
+            if self._serial_object.in_waiting:
+                try:
+                    (raw_data, parsed_data) = self._ubxreader.read()
+#                     if raw_data:
+#                         print(raw_data)
+                    if parsed_data:
+                        print(parsed_data)
+                        # print(raw_data)
+                        print(raw_data.hex(sep=' ', bytes_per_sep=2))
+                except (ube.UBXStreamError, ube.UBXMessageError, ube.UBXTypeError,
+                        ube.UBXParseError) as err:
+                    print(f"Something went wrong {err}")
+                    continue
