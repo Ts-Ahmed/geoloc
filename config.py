5f19a69582a07d8748b3ed14fa9318e76c9bf34c
Ahmed Tsoroev <ts.ahmed.edu@gmail.com>
2021-01-10 23:05:59 +0100

    Made separate files to extract relevant data easily.
    Signed-off-by: Ahmed Tsoroev <ts.ahmed.edu@gmail.com>

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 MU = 3.986005 * (10 ** 14)
 OMEGA_E_DOT = 7.292115 * (10 ** -5)
 # set PORT, BAUDRATE and TIMEOUT as appropriate
-PORT = 'COM5'
+PORT = 'COM11'
 BAUDRATE = 9600
 TIMEOUT = 0
 
---
+++ getalmanac.py
@@ -0,0 +1,27 @@
+from time import sleep
+from pyubx2 import UBXMessage, POLL
+
+from config import PORT, TIMEOUT, BAUDRATE
+from ubxstreamer import UBXStreamer
+
+if __name__ == "__main__":
+
+    print("Instantiating UBXStreamer class...")
+    ubxs = UBXStreamer(PORT, BAUDRATE, TIMEOUT)
+    print(f"Connecting to serial port {PORT} at {BAUDRATE} baud...")
+    ubxs.connect()
+    print("Starting reader thread...")
+    ubxs.start_read_thread()
+
+    print("\nPolling receiver...\n\n")
+    msg1 = UBXMessage('NAV', 'NAV-TIMEGPS', POLL)
+    msg2 = UBXMessage('AID', 'AID-ALM', POLL)
+    ubxs.send(msg1.serialize(), msg2.serialize())
+
+    sleep(3)
+
+    print("\n\nStopping reader thread...")
+    ubxs.stop_read_thread()
+    print("Disconnecting from serial port...")
+    ubxs.disconnect()
+    print("Done")
